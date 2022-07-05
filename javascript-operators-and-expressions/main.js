de78ee8f358e7fda22a667df686f326b4f5aebb1
jghil <jayjeonghil94@gmail.com>
2022-07-05 19:41:41 +0000

    js-operators-and-expressions - Jay Ghil

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,39 @@
+var width = 9;
+var height = 10;
+var area = width * height;
+console.log('Value of area:', area);
+console.log('typeof for area:', typeof area);
+
+var bill = 90;
+var payment = 93;
+var change = payment - bill;
+console.log('Value of change:', change);
+console.log('typeof for change:', typeof change);
+
+var quizzes = 95;
+var midterm = 93;
+var final = 90;
+var grade = (quizzes + midterm + final) / 3;
+console.log('Value of grade:', grade);
+console.log('typeof for grade:', typeof grade);
+
+var firstName = 'Jay';
+var lastName = 'Ghil';
+var fullName = firstName + ' ' + lastName;
+console.log('Value of fullName:', fullName);
+console.log('typeof for fullName:', typeof fullName);
+
+var pH = 39;
+var isAcidic = pH > 7;
+console.log('Value of isAcidic:', isAcidic);
+console.log('typeof for isAcidic:', typeof isAcidic);
+
+var headCount = 21;
+var isSparta = headCount === 300;
+console.log('Value of isSparta', isSparta);
+console.log('typeof for isSparta', typeof isSparta);
+
+var motto = 'Jay Ghil';
+motto = motto + ' is the GOAT';
+console.log('Value of motto:', motto);
+console.log('typeof for motto:', typeof motto);
