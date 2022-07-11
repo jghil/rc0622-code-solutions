575893683b66bad6170eb2b51ba39512726e8b40
jghil <jayjeonghil94@gmail.com>
2022-07-11 16:43:04 +0000

    object-manipulation-1 - Jay Ghil

---
+++ object-manipulation-1/src/get-value.js
@@ -0,0 +1,9 @@
+/* exported getValue */
+
+// return the object and the object's key
+
+function getValue(object, key) {
+  return (object, object[key]);
+}
+
+// return the value in object found at the key property
