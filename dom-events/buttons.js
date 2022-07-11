08e71372fb2054910419053b42cbe8daa886b1a6
jghil <jayjeonghil94@gmail.com>
2022-07-11 20:13:18 +0000

    dom-events - Jay Ghil

---
+++ dom-events/buttons.js
@@ -0,0 +1,23 @@
+function handleClick(event) {
+  console.log('button clicked');
+  console.log('event:', event);
+  console.log('event.target:', event.target);
+}
+var $button = document.querySelector('.click-button');
+$button.addEventListener('click', handleClick);
+
+function handleMouseOver(event) {
+  console.log('button hovered');
+  console.log('event:', event);
+  console.log('target:', event.target);
+}
+var $hoverButton = document.querySelector('.hover-button');
+$hoverButton.addEventListener('mouseover', handleMouseOver);
+
+function handleDoubleClick(event) {
+  console.log('button double-clicked');
+  console.log('event:', event);
+  console.log('target:', event.target);
+}
+var $doubleClick = document.querySelector('.double-click-button');
+$doubleClick.addEventListener('dblclick', handleDoubleClick);
