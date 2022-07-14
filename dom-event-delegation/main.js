9e051f0316b736d39f564669c0322ebf4a3135da
jghil <jayjeonghil94@gmail.com>
2022-07-14 18:40:37 +0000

    dom-event-delegation

---
+++ dom-event-delegation/main.js
@@ -0,0 +1,11 @@
+var $taskList = document.querySelector('.task-list');
+
+$taskList.addEventListener('click', function (event) {
+  console.log('event.target:', event.target);
+  console.log('event.target.tagName:', event.target.tagName);
+  if (event.target && event.target.nodeName === 'BUTTON') {
+    var $closestElement = event.target.closest('.task-list-item');
+    console.log($closestElement);
+    $closestElement.remove($closestElement);
+  }
+});
